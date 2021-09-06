8bd14fab92c76ec8860b7cf701b8b504fca3e990
HifsaAnsari <72120217+HifsaAnsari@users.noreply.github.com>
2021-09-07 01:40:06 +0500

    Update Slot.js

--- src/component/Slot.js
+++ src/component/Slot.js
@@ -4,7 +4,7 @@ export default class Slot extends Component {
   constructor(props){
     super(props);
     this.state={
-      winner:null,
+      winner:,
     }
   }
     isWinner(symbol1, symbol2, symbol3) {
@@ -12,8 +12,10 @@ export default class Slot extends Component {
             }
            
             handleClick = () => {
-          this.setState( console.log("hi"))
-              console.log(`you: ${ this.state.winner}`);
+          this.setState(()=>{
+            console.log(`you: ${ this.state.winner}`);
+          })
+            
             }
            
     render() {
